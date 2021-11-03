c4db1ff45e9ef27dc6384f25b07a01a603fd5e81
Andres <aneduardotejada@gmail.com>
2021-11-03 14:11:17 -0500

    search component finished

--- src/components/app/pages/CheckInformation/CheckInformation.tsx
+++ src/components/app/pages/CheckInformation/CheckInformation.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Alert, Input, Space } from 'antd';
+import { useState } from 'react';
+import Spinner from '../../../layout/Spinner/Spinner';
+const { Search } = Input;
 
 const CheckInformation = () => {
+    const [isLoading, setIsLoading] = useState<Boolean>(false);
+    const [msg, setMsg] = useState<String>('');
+    const [error,setError]=useState<boolean>(false)
+    const onSearch = (value:string) => {
+        setIsLoading(true)
+        if(value.trim() === ""){
+            setMsg('Debes ingresar un termino de busqueda');
+            setIsLoading(false);
+            setError(true)
+            return
+        }
+        setError(false)
+        setIsLoading(false)
+    }
+
     return (
         <div>
-            <p>Pagina de consultas</p>
+            <Search
+                placeholder="input search text"
+                allowClear
+                enterButton="Search"
+                size="large"
+                onSearch={onSearch}
+            />
+            {
+                isLoading
+                ? <Spinner />
+                :null
+            }
+            {
+                error
+                ?              
+                <Alert className='custom-alert' message={msg} type="error" showIcon />
+                
+                :null
+
+            }
         </div>
     )
 }
